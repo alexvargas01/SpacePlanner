1129f129a33ca23ed4e9f101520f9d01109ca408
CesarMtzV <mc.cesar.art@gmail.com>
2020-08-29 21:04:15 -0500

    Working on decreasing image opacity on click

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("Hello world!");
-
 const form = document.querySelector("form");
 
 form.addEventListener("submit", event => {
@@ -22,9 +20,22 @@ form.addEventListener("submit", event => {
   document.getElementById("seat-map").style.height = `${containerHeight}px`;
 
   for (var i = 0; i < rows * columns; i++) {
-    var v = document.createElement("img");
-    v.src = "images/seat.png";
-    v.className = "seat-image";
+    var seatImage = document.createElement("img");
+    var anchor = document.createElement("a");
+
+    seatImage.src = "images/seat.png";
+    seatImage.className = "seat-image";
+    seatImage.id = "seat-image";
+
+    anchor.onclick = "lowerOpacity()";
     document.getElementById("seat-map").appendChild(v);
   }
+  //El aglortimo da una opcion de como acomodar los asietnos, pero el usuario puede modificarlo al final
+  //Al darle click a un asiento se opaca (para simbolizar que no se va a usar)
+  //Al darle click de nuevo se opaca aun más para simbolizar que ahí no hay butaca
 });
+
+function lowerOpacity() {
+  console.log("Opacity lowered");
+  document.getElementById("seat-image").style.opacity = "0.5";
+}
